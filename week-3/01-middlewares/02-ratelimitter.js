f49837e543b80aeb7cc17edffc1fa247a8dc6cc8
vermaji <rajaditya14032002@gmail.com>
2024-01-16 00:41:49 +0530

    changed folder structure

---
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -0,0 +1,39 @@
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
+const app = express();
+// You have been given an express server which has a few endpoints.
+// Your task is to create a global middleware (app.use) which will
+// rate limit the requests from a user to only 5 requests per second.
+// If a user sends more than 5 requests in a single second, the server
+// should block them with a 404.
+// User will be sending in their user id in the header as 'user-id'
+// You have been given a numberOfRequestsForUser object to start off with which
+// clears every one second
+
+app.use((req, res, next) => {
+  console.log(new Date().getMilliseconds());
+  for (let i = 0; i < 2; i++) {
+    (async () => {
+      await fetch("http://localhost:3000/user").then(() => {
+        console.log(`done for ${i} time`);
+      });
+    })();
+  }
+  next();
+});
+
+let numberOfRequestsForUser = {};
+setInterval(() => {
+  numberOfRequestsForUser = {};
+}, 1000);
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
+});
+
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
+});
+app.listen(3000);
+module.exports = app;
