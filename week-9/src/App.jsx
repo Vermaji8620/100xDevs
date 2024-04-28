105c0b5e087dcf49720da898e7f380411bcb278a
vermaji <rajaditya14032002@gmail.com>
2024-04-28 10:38:15 +0530

    delegating task to some other hook done

--- week-9/src/App.jsx
+++ week-9/src/App.jsx
@@ -1,66 +1,25 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-const useTodos = (n) => {
-  const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState(true);
-  console.log(todos);
+const useInterval = () => {
+  const [timer, setTimer] = useState(0);
 
   useEffect(() => {
-    const value = setInterval(() => {
-      axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-        setTodos(res.data.todos);
-        setLoading(false);
-      });
-    }, n * 1000);
-
-    axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-      setTodos(res.data.todos);
-      setLoading(false);
-    });
-
-    // lets now say that the n is changed, now the previous n that was already running needs to be stopped for the new n to take place (we should not have two clocks running at the same time)and have all the effects, so need to clear the earlier intervals--- so use unmounting concept
+    const iner = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
     return () => {
-      clearInterval(value);
+      clearInterval(iner);
     };
+  }, [timer]);
 
-    // flow for mounting and unmounting ----
-    // for the first time when the useffect is run inside this function, the unmounting concept does not happen, but when the n value is changed, first it goes to the demounting to remove any interval that is already running and then, goes on to mount the newly created thing
-  }, [n]);
-
-  return { todos, loading };
+  return timer;
 };
 
 const App = () => {
-  const { todos, loading } = useTodos(3);
-  return (
-    <div>
-      {loading ? (
-        <div>loading...</div>
-      ) : (
-        todos.map((todo, index) => (
-          <div key={index}>
-            <Track todo={todo} />
-          </div>
-        ))
-      )}
-    </div>
-  );
-};
+  const timer = useInterval();
 
-import PropTypes from "prop-types";
-
-const Track = ({ todo }) => {
-  return (
-    <div>
-      {todo.title}
-      {todo.description}
-    </div>
-  );
-};
-
-Track.propTypes = {
-  todo: PropTypes.object.isRequired,
+  return <div>Timer is at {timer}</div>;
 };
 
 export default App;
