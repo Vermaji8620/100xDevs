228d8df1a1b2b4b1b5ceb4624c3836c68ad724ee
vermaji <rajaditya14032002@gmail.com>
2024-04-29 07:57:27 +0530

    function explicit type mention

--- week-9/ts/a.js
+++ week-9/ts/a.js
@@ -1,5 +1,13 @@
 "use strict";
-const greet = (a, b) => {
-    return a + b;
+// cCreate a function that takes another function as input, and runs it after 1 second.
+const torun = () => {
+    console.log("running");
+    return 3;
 };
-console.log(greet(7, 10));
+const func = (tunrun) => {
+    setTimeout(() => {
+        const fdfd = tunrun();
+        console.log(fdfd);
+    }, 3000);
+};
+func(torun);
--- week-9/ts/a.ts
+++ week-9/ts/a.ts
@@ -1,5 +1,15 @@
-const greet = (a: number, b: number) => {
-    return ("dsfsdf" + a + b);
+// cCreate a function that takes another function as input, and runs it after 1 second.
+
+const torun = () => {
+    console.log("running")
+    return 3;
 }
 
-console.log(greet(7, 10))
+const func = (tunrun: () => number) => {
+    setTimeout(() => {
+        const fdfd = tunrun();
+        console.log(fdfd)
+    }, 3000);
+}
+
+func(torun)
