50551e1d81c019fc71bba685670053811940496f
vermaji <rajaditya14032002@gmail.com>
2024-04-29 07:42:33 +0530

    started learning typescript

---
+++ week-9/ts/a.js
@@ -0,0 +1,5 @@
+"use strict";
+const greet = (a, b) => {
+    return a + b;
+};
+console.log(greet(7, 10));
---
+++ week-9/ts/a.ts
@@ -0,0 +1,5 @@
+const greet = (firstname: any, lastname: string) => {
+    console.log("hello " + firstname + lastname)
+}
+
+greet(7, "ags")
