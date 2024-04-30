2bfcc86b04be0afb1fefe9b53a7ed8c68f2c6ab6
vermaji <rajaditya14032002@gmail.com>
2024-04-30 10:29:20 +0530

    union and intersections

--- week-9/ts/src/a.ts
+++ week-9/ts/src/a.ts
@@ -1,10 +1,33 @@
 //  type lets us define the optional data types , interface doesn't
 
-type GreetArg = number | string
+// type GreetArg = number | string
 
-const greet = (id: GreetArg) => {
-    console.log("thjhfjdhfjdsa" + id)
-}
+// const greet = (id: GreetArg) => {
+//     console.log("thjhfjdhfjdsa" + id)
+// }
 
-greet(5245)
-greet("4545454")
+// greet(5245)
+// greet("4545454")
+
+// union and intersection
+
+// type Employee = {
+//     name: string,
+//     startDate: Date
+// }
+
+// interface Manager {
+//     name: string,
+//     department: string
+// }
+
+// type TechLead = Employee & Manager
+// type TechLead = Employee | Manager
+
+
+// const t: TechLead = {
+//     name: "adjhasgd",
+//     startDate: new Date(),
+//     department: "hsdfgds"
+// }
+
