110e8421d7ba0441a8ee800f34a5bdbedea8bf11
vermaji <rajaditya14032002@gmail.com>
2024-04-30 11:15:37 +0530

    array type

--- week-9/ts/dist/a.js
+++ week-9/ts/dist/a.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log(":fhdshgf");
+//  type lets us define the optional data types , interface doesn't
+const filterfunc = (arrinput) => {
+    return arrinput.filter(each => each.age >= 16);
+};
+console.log(filterfunc([{ name: 'asdasda', class: 'afaas', age: 12 }, { name: 'SEEE', class: 'SSSSSSS', age: 15 }, { name: 'XXX', class: 'XXXXXX', age: 17 }, { name: 'ZZZZ', class: 'ZZZZZZZZ', age: 16 }]));
--- week-9/ts/src/a.ts
+++ week-9/ts/src/a.ts
@@ -21,7 +21,9 @@
 //     department: string
 // }
 
+// for intersection
 // type TechLead = Employee & Manager
+// for union
 // type TechLead = Employee | Manager
 
 
@@ -31,3 +33,32 @@
 //     department: "hsdfgds"
 // }
 
+
+// //////////////////////////    everytime use type because it goes with everything , even for union and intersection, just for implementing to classes we use interface
+
+// arrays in TS
+// max value of a number
+// function maxValue(arr: number[]) {
+// type num = number[];
+// function maxValue(arr: num) {
+//     let max = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i]
+//         }
+//     }
+//     return max;
+// }
+// console.log(maxValue([1, 2, 3]));
+
+type allarray = {
+    name: string, class: string, age: number
+}
+
+const filterfunc = (arrinput: allarray[]) => {
+
+    return arrinput.filter(each => each.age >= 16)
+
+}
+
+console.log(filterfunc([{ name: 'asdasda', class: 'afaas', age: 12 }, { name: 'SEEE', class: 'SSSSSSS', age: 15 }, { name: 'XXX', class: 'XXXXXX', age: 17 }, { name: 'ZZZZ', class: 'ZZZZZZZZ', age: 16 }]))
