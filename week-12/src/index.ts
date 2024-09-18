dee198d9224beaca243cfc4d461a8b08d309df08
AdityaRaj <xyz@gmai.com>
2024-09-18 13:38:19 +0530

    pick and partial done

--- week-12/src/index.ts
+++ week-12/src/index.ts
@@ -1,10 +1,40 @@
+// interface User {
+//     name: string
+//     age: number
+// }
+
+// function sumofage(user1: User, user2: User) {
+//     return user1.age + user2.age
+// }
+
+// console.log(sumofage({ name: "aditya", age: 36 }, { name: "verma", age: 45 }))
+
+
 interface User {
-    name: string
-    age: number
+    id: string,
+    name: string,
+    age: string,
+    email: string,
+    password: string,
 }
 
-function sumofage(user1: User, user2: User) {
-    return user1.age + user2.age
+// for selecting particular fields
+type updateProps = Pick<User, 'name' | 'email' | 'age'>
+
+// for making those fields optional
+type updatePropsOptional = Partial<updateProps>
+
+
+// function updateUser(updatedProps: updateProps) {
+//     console.log(updatedProps.name + " " + updatedProps.email + " " + updatedProps.age)
+// }
+
+function updateUser(updatedProps: updatePropsOptional) {
+    console.log(updatedProps.name + " " + updatedProps.email + " " + updatedProps.age)
 }
 
-console.log(sumofage({ name: "aditya", age: 36 }, { name: "verma", age: 45 }))
+updateUser(
+    {
+        name: "adadjadajdh"
+    }
+)
